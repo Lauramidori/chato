78d4c1f94e4d3b780d3ec339f78ffe1d6b0f31cd
Lauramidori <lauramidori6@gmail.com>
2019-04-29 12:17:22 -0300

    teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  val1: number = 0;
+  val2: number = 0;
+  val: number;
+
+somar ():void{
+  let total = Number(this.val1) + Number(this.val2);
+  alert ("A soma é:"+total);
+  if (total %2 == 0){
+    alert("É par")
+  } else{
+    alert("É impar")
+  }
+}
 }
